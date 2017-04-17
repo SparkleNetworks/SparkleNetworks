bc499c9689000727dd36c62e26dc70bb015af39b
sandrock <antoine.sottiau@gmail.com>
2017-04-17 22:15:22 +0200

    Sparkle.Common

---
+++ src/Sparkle.Common/NativeMethods.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Sparkle.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Runtime.InteropServices;
+    using System.IO;
+
+    /// <summary>
+    /// Calls to native system methods.
+    /// </summary>
+    public static class NativeMethods
+    {
+        //
+        // INFORMATION
+        //
+        // Most of this class has moved to SrkToolkit.Common
+        //
+    }
+}
