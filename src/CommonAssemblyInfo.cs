69d77a7d1f7fee56e86c6ab3c1752943c3d0dd7d
sandrock <antoine.sottiau@gmail.com>
2017-04-17 20:30:01 +0200

    minors files

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+#if SSC
+[assembly: AssemblyCompany(SparkleSystems.Configuration.AppVersion.Company)]
+[assembly: AssemblyProduct(SparkleSystems.Configuration.AppVersion.Name)]
+[assembly: AssemblyCopyright(SparkleSystems.Configuration.AppVersion.Copyright)]
+
+[assembly: AssemblyVersion(SparkleSystems.Configuration.AppVersion.Full)]
+[assembly: AssemblyFileVersion(SparkleSystems.Configuration.AppVersion.Full)]
+#else
+[assembly: AssemblyCompany(Sparkle.Infrastructure.AppVersion.Company)]
+[assembly: AssemblyProduct(Sparkle.Infrastructure.AppVersion.Name)]
+[assembly: AssemblyCopyright(Sparkle.Infrastructure.AppVersion.Copyright)]
+
+[assembly: AssemblyVersion(Sparkle.Infrastructure.AppVersion.Full)]
+[assembly: AssemblyFileVersion(Sparkle.Infrastructure.AppVersion.Full)]
+#endif
---
+++ src/CommonAssemblyInfoInternal.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+[assembly: AssemblyCompany(Sparkle.Infrastructure.AppVersionInternal.Company)]
+[assembly: AssemblyProduct(Sparkle.Infrastructure.AppVersionInternal.Name)]
+[assembly: AssemblyCopyright(Sparkle.Infrastructure.AppVersionInternal.Copyright)]
+
+[assembly: AssemblyVersion(Sparkle.Infrastructure.AppVersionInternal.Full)]
+[assembly: AssemblyFileVersion(Sparkle.Infrastructure.AppVersionInternal.Full)]
